df09b3363c7b96773f9d2bee1a0f33266bbcf395
Austin L. Howard <contact@austinlhoward.com>
2026-05-25 18:47:00 -0600

    Implement PPU status register read masking

--- PPU.cpp
+++ PPU.cpp
@@ -2,6 +2,11 @@
 #include <cstring>
 
 
+const uint8_t STATUS_VBLANK_MASK = 0x80;
+const uint8_t STATUS_SPRITE_ZERO_HIT_MASK = 0x40;
+const uint8_t STATUS_SPRITE_OVERFLOW_MASK = 0x20;
+
+
 PPU::PPU() {
 	for (auto& nt : nameTable)
 		for (auto& b : nt) b = 0x00;
@@ -28,76 +33,22 @@ uint8_t PPU::cpuRead(uint16_t addr, bool bReadOnly) {
 		break;
 	case 0x0002: // Status
 		data = (status & 0xE0) | (dataBuffer & 0x1F);
-		status &= ~0x80; // Clear vblank flag on read
+		status &= ~STATUS_VBLANK_MASK; // Clear vblank flag on read
 		bAddressLatch = false;
 		break;
 	case 0x0003: // OAM Address
-		// OAMADDR register (W)
 		break;
 	case 0x0004: // OAM Data
-		data = oamMemory[oamAddr];
+		data = oamMemory[oamAddress];
 		break;
 	case 0x0005: // Scroll
 		break;
 	case 0x0006: // PPU Address
 		break;
 	case 0x0007: // PPU Data
+		data = dataBuffer;
 		break;
 	}
 
 	return data;
-}
-
-void PPU::cpuWrite(uint16_t addr, uint8_t data) {
-	switch (addr) {
-	case 0x0000: // Control
-		control = data;
-		break;
-	case 0x0001: // Mask
-		mask = data;
-		break;
-	case 0x0002: // Status
-		break;
-	case 0x0003: // OAM Address
-		oamAddr = data;
-		break;
-	case 0x0004: // OAM Data
-		oamMemory[oamAddr] = data;
-		oamAddr++; // Auto increment after write
-		break;
-	case 0x0005: // Scroll
-		break;
-	case 0x0006: // PPU Address
-		break;
-	case 0x0007: // PPU Data
-		break;
-	}
-}
-
-void PPU::ppuWrite(uint16_t addr, uint8_t data) {
-	addr &= 0x3FFF;
-	if (addr >= 0x0000 && addr <= 0x1FFF) {
-		patternTable[(addr & 0x1000) >> 12][addr & 0x0FFF] = data;
-	}
-	else if (addr >= 0x2000 && addr <= 0x3EFF) {
-		addr &= 0x0FFF;
-		if (cart->mirror == Cartridge::MIRROR::VERTICAL) {
-			// Vertical Mirroring
-			if (addr >= 0x0000 && addr <= 0x03FF) nameTable[0][addr & 0x03FF] = data;
-			if (addr >= 0x0400 && addr <= 0x07FF) nameTable[1][addr & 0x03FF] = data;
-			if (addr >= 0x0800 && addr <= 0x0BFF) nameTable[0][addr & 0x03FF] = data;
-			if (addr >= 0x0C00 && addr <= 0x0FFF) nameTable[1][addr & 0x03FF] = data;
-		}
-		else if (cart->mirror == Cartridge::MIRROR::HORIZONTAL) {
-			// Horizontal Mirroring
-			if (addr >= 0x0000 && addr <= 0x03FF) nameTable[0][addr & 0x03FF] = data;
-			if (addr >= 0x0400 && addr <= 0x07FF) nameTable[0][addr & 0x03FF] = data;
-			if (addr >= 0x0800 && addr <= 0x0BFF) nameTable[1][addr & 0x03FF] = data;
-			if (addr >= 0x0C00 && addr <= 0x0FFF) nameTable[1][addr & 0x03FF] = data;
-		}
-	}
-	else if (addr >= 0x3F00 && addr <= 0x3F1F) {
-		addr &= 0x001F;
-		paletteTable[addr] = data;
-	}
 }
